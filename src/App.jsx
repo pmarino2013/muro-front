271dcf45b4163f25a6686b3332000d1f57b3d49c
Pablo Marino <pmarino2013@gmail.com>
2026-07-28 18:35:56 +0000

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Muro de los deseos</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
