ed42d45db8da792ac224f1ea4d06ccc522ba6ca1
Pablo Marino <pmarino2013@gmail.com>
2026-07-29 22:30:51 -0300

    Preparado para vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const socket = io("https://muro-back.vercel.app");
+const socket = io();
 
 const App = () => {
   const [mensajes, setMensajes] = useState([]);
